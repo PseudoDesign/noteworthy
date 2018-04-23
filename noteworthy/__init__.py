5e5cb8839bd6b4fd04b52b0ac026ac3cd0e91b59
Adam <PseudoDesign@users.noreply.github.com>
2018-04-23 00:58:58 -0600

    able to install and run

--- noteworthy/__init__.py
+++ noteworthy/__init__.py
@@ -4,15 +4,19 @@ from time import sleep
 import argparse
 
 
+def run(username, working_directory):
+    while True:
+        network = lastfm.connect(keys.API_KEY, keys.SECRET_KEY)
+        track = lastfm.current_track(network, username)
+        lastfm.save_track_info(track, working_directory)
+        sleep(5)
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("username")
+    parser.add_argument("working_directory")
     args = parser.parse_args()
-    print(args.echo)
 
-    while True:
-        network = lastfm.connect(keys.API_KEY, keys.SECRET_KEY)
-        track = lastfm.current_track(network, "PseudoDesign")
-        lastfm.save_track_info(track, r'C:\Users\Adam\Documents\Twobbler')
-        sleep(5)
+    run(args.username, args.working_directory)
 
