d111b96db151553637c2ac752a13fba63905a6a6
Adam <PseudoDesign@users.noreply.github.com>
2018-04-23 00:31:24 -0600

    removing junk

--- noteworthy/__init__.py
+++ noteworthy/__init__.py
@@ -1,9 +1,15 @@
 from . import lastfm
 from . import keys
 from time import sleep
+import argparse
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("username")
+    args = parser.parse_args()
+    print(args.echo)
+
     while True:
         network = lastfm.connect(keys.API_KEY, keys.SECRET_KEY)
         track = lastfm.current_track(network, "PseudoDesign")
